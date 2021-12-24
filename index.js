d6d2d7a76792e9314042ad4a31660993658f9a7a
Marsel <you@example.com>
2021-12-24 09:34:48 +0100

    commit

--- index.js
+++ index.js
@@ -172,6 +172,158 @@ function renderTiTac4x4() {
 
 }
 
+function renderTiTac3x3() {
+
+    const containerEl = document.createElement('div')
+    containerEl.setAttribute('id', 'container')
+
+    // Starting Page
+
+    const startingPage = document.createElement('div')
+    startingPage.setAttribute('id', 'startingPage')
+
+    const titlegame = document.createElement('h2')
+    titlegame.textContent = 'Tic Tac Toe'
+
+    const divLine = document.createElement('div')
+    divLine.setAttribute('id', 'line')
+
+    const selectTitle = document.createElement('h4')
+    selectTitle.textContent = 'Select Which You Want To Be?'
+
+    const buttonDiv = document.createElement('div')
+    buttonDiv.setAttribute('id', 'button')
+
+    const buttonPlayerx = document.createElement('button')
+    buttonPlayerx.setAttribute('id', 'playerX')
+    buttonPlayerx.setAttribute('class', 'choose')
+    buttonPlayerx.textContent = 'Player ( X )'
+
+    const buttonPlayero = document.createElement('button')
+    buttonPlayero.setAttribute('id', 'playerO')
+    buttonPlayero.setAttribute('class', 'choose')
+    buttonPlayero.textContent = 'Player ( O )'
+
+    buttonDiv.append(buttonPlayerx, buttonPlayero)
+
+    startingPage.append(titlegame, divLine, selectTitle, buttonDiv)
+
+
+
+    // <!-- Main Page -->
+    // <div id="mainPage">
+    //     <div id="headerBtns">
+    //         <button id="">X Turn</button>
+    //         <button id="O_Turn">O Turn</button>
+    //         <div id="showChange"></div>
+    //     </div>
+    //     <div id="gameBoard">
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //         <div class="boxes"></div>
+    //     </div>
+    // </div>
+
+    // Main Page
+
+    const mainPage = document.createElement('div')
+    mainPage.setAttribute('id', 'mainPage')
+
+    const headerButton = document.createElement('div')
+    headerButton.setAttribute('id', 'headerBtns')
+
+    const buttonX = document.createElement('button')
+    buttonX.setAttribute('id', '')
+    buttonX.textContent = 'X Turn'
+
+    const buttonO = document.createElement('button')
+    buttonO.setAttribute('id', 'O_Turn')
+    buttonO.textContent = 'O Turn'
+
+    const showChange = document.createElement('div')
+    showChange.setAttribute('id', 'showChange')
+
+    headerButton.append(buttonX, buttonO, showChange)
+
+
+
+    const gameBoard = document.createElement('div')
+    gameBoard.setAttribute('id', 'gameBoard')
+
+    const divBoxes1 = document.createElement('div')
+    divBoxes1.setAttribute('class', 'boxes')
+
+    const divBoxes2 = document.createElement('div')
+    divBoxes2.setAttribute('class', 'boxes')
+
+    const divBoxes3 = document.createElement('div')
+    divBoxes3.setAttribute('class', 'boxes')
+
+    const divBoxes4 = document.createElement('div')
+    divBoxes4.setAttribute('class', 'boxes')
+
+    const divBoxes5 = document.createElement('div')
+    divBoxes5.setAttribute('class', 'boxes')
+
+    const divBoxes6 = document.createElement('div')
+    divBoxes6.setAttribute('class', 'boxes')
+
+    const divBoxes7 = document.createElement('div')
+    divBoxes7.setAttribute('class', 'boxes')
+
+    const divBoxes8 = document.createElement('div')
+    divBoxes8.setAttribute('class', 'boxes')
+
+    const divBoxes9 = document.createElement('div')
+    divBoxes9.setAttribute('class', 'boxes')
+
+    gameBoard.append(divBoxes1, divBoxes2, divBoxes3, divBoxes4, divBoxes5, divBoxes6, divBoxes7, divBoxes8, divBoxes9)
+
+
+    mainPage.append(headerButton, gameBoard)
+
+
+    // <!-- WInner Page -->
+    // <div id="winner">
+    //     <h2 id="winnerName">Player X Win The Game!</h2>
+    //     <div id="button">
+    //         <button id="quit">Play Again</button>
+    //     </div>
+    // </div>
+
+    // WInner Page
+
+    const divWiner = document.createElement('div')
+    divWiner.setAttribute('id', 'winner')
+
+    const winerName = document.createElement('h2')
+    winerName.setAttribute('id', 'winnerName')
+    winerName.textContent = 'Player X Win The Game!'
+
+    const btnPlay = document.createElement('div')
+    btnPlay.setAttribute('id', 'button')
+
+    const btnQuit = document.createElement('button')
+    btnQuit.setAttribute('id', 'quit')
+    btnQuit.textContent = 'Play Again'
+
+    btnPlay.append(btnQuit)
+
+    divWiner.append(winerName, btnPlay)
+
+
+    containerEl.append(startingPage, mainPage, divWiner)
+
+    document.body.append(containerEl)
+
+}
+
 function render() {
     document.body.innerHTML = ''
     renderTiTac4x4()
